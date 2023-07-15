bba524e252496b4340f8b81b673ef27a5964a254
Robson William <robsonwilliamro@gmail.com>
2023-07-15 17:27:33 -0300

    Adicionando redis e socket.io.
    - Adicionando as dependências redis e socket.io.
    - Configurando socket.io no aquivo socketIo.ts.
    - Adicionando a executação do socket ao servidor.

--- src/envConfig.ts
+++ src/envConfig.ts
@@ -1,4 +1,8 @@
 import { config } from "dotenv";
 config();
 
-export default { PORT: process.env.PORT };
+export default {
+  PORT: process.env.PORT || undefined,
+  CLIENT_URL: process.env.CLIENT_URL || undefined,
+  REDIS_HOST: process.env.REDIS_HOST || undefined
+};
